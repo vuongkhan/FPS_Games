ecd6804925d943da6d2ad4fd83203801326bbb28
vuongkhan <davidvxag2001@gmail.com>
2025-05-29 17:20:08 +0700

    Apply .gitignore to existing tracked files

---
+++ Assets/Scripts/AI/EnemyBase.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(BlackboardBase))]
+public class EnemyBase : MonoBehaviour
+{
+    public BlackboardBase blackboard { get; private set; }
+    public EnemyFSMController fsmController { get; private set; }
+
+    private void Awake()
+    {
+        GetComponent<EnemyBlackboardInitializer>()?.Initialize();
+        blackboard = GetComponent<BlackboardBase>();
+        if (blackboard == null)
+        {
+            Debug.LogError("Blackboard is missing on " + gameObject.name);
+        }
+        fsmController = new EnemyFSMController();
+    }
+
+    protected virtual void Start()
+    {
+        if (fsmController != null)
+        {
+            fsmController.ChangeState(new EnemyPatrolState(this));
+        }
+        else
+        {
+            Debug.LogError("FSM Controller is null on " + gameObject.name);
+        }
+    }
+
+    protected virtual void Update()
+    {
+        UpdateVision();
+        fsmController?.Update();
+        SeeEnemy();        
+    }
+
+    private void UpdateVision()
+    {
+        if (Target == null)
+        {
+            blackboard?.Set(BBKeys.CanSeeEnemy, false);
+            return;
+        }
+
+        float distance = Vector3.Distance(transform.position, Target.transform.position);
+        blackboard?.Set(BBKeys.CanSeeEnemy, distance <= 10f);
+    }
+    protected virtual void SeeEnemy() { }
+    public GameObject Target => blackboard?.Get<GameObject>(BBKeys.Target);
+    public bool CanSeeEnemy => blackboard?.Get<bool>(BBKeys.CanSeeEnemy) ?? false;
+}
---
+++ Assets/Scripts/AI/Range_Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Range_Enemy : EnemyBase
+{
+    [SerializeField] private Transform firePoint;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+    public void RangeAOEAttack(GameObject projectileToShoot)
+    {
+        if (projectileToShoot == null)
+            return;
+        Quaternion baseRotation = Quaternion.LookRotation(firePoint.forward);
+        Quaternion finalRotation = baseRotation * Quaternion.Euler(0, -90f, 0);
+        GameObject proj = Instantiate(projectileToShoot, firePoint.position, finalRotation);
+    }
+
+
+
+    public void SpawnProjectileAtTarget(GameObject projectileToSpawn)
+    {
+        if (projectileToSpawn == null)
+        {
+            return;
+        }
+
+        if (Target == null)
+        {
+            return;
+        }
+        Vector3 spawnPosition = Target.transform.position;
+        Quaternion spawnRotation = Quaternion.identity;
+
+        Instantiate(projectileToSpawn, spawnPosition, spawnRotation);
+    }
+
+}
